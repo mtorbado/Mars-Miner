ede9564d5293c96c3aa22e58ae2905ee90fb57df
martorb <37657334+zombivoro@users.noreply.github.com>
2020-03-02 21:49:11 +0100

    first commit

---
+++ Assets/Scripts/DragObject.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DragObject : MonoBehaviour
+{
+
+    Rigidbody rb;
+    private Ray mouseRay, cubeRay;
+    public Plane basePlane;
+
+    private void Start()
+    {
+        Vector3 normal;
+        rb = GetComponent<Rigidbody>();
+        var filter = GameObject.Find("Plane").GetComponent<MeshFilter>();
+
+        normal = filter.transform.TransformDirection(filter.mesh.normals[0]);
+        basePlane = new Plane(normal, transform.position);
+    }
+
+    void OnMouseDown()
+    {
+
+    }
+
+    private void OnMouseDrag()
+    {
+        mouseRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+        if (basePlane.Raycast(mouseRay, out float distance))
+        {
+            // get the hit point:
+            transform.position = mouseRay.GetPoint(distance);
+        }
+    }
+
+    private void OnMouseUp()
+    {
+        
+
+    }
+}
+
