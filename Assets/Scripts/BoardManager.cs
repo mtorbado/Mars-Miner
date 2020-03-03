bdf1adc3e7fe76c2fabc272325a843c33771dd47
zombivoro <mariotorbado.96@gmail.com>
2020-03-03 12:09:55 +0100

    add board script and some DragObject script experiments

---
+++ Assets/Scripts/BoardManager.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoardManager : MonoBehaviour
+{
+
+    private const float TILE_SIZE = 1.0f;
+    private const float TILE_OFFSET = 0.5f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        DrawBoard();
+    }
+
+    private void DrawBoard()
+    {
+        Vector3 xLine = Vector3.right * 20;
+        Vector3 zLine = Vector3.forward * 20;
+
+        for (int i = 0; i <= 20; i++)
+        {
+            Vector3 start = Vector3.forward * i;
+            Debug.DrawLine(start, start + xLine);
+            for (int j = 0; j <= 20; j++)
+            {
+                start = Vector3.right * j;
+                Debug.DrawLine(start, start + zLine);
+            }
+
+        }
+    }
+}
--- Assets/Scripts/DragObject.cs
+++ Assets/Scripts/DragObject.cs
@@ -6,17 +6,11 @@ public class DragObject : MonoBehaviour
 {
 
     Rigidbody rb;
-    private Ray mouseRay, cubeRay;
     public Plane basePlane;
 
     private void Start()
     {
-        Vector3 normal;
-        rb = GetComponent<Rigidbody>();
-        var filter = GameObject.Find("Plane").GetComponent<MeshFilter>();
-
-        normal = filter.transform.TransformDirection(filter.mesh.normals[0]);
-        basePlane = new Plane(normal, transform.position);
+        basePlane = GetBasePlane();
     }
 
     void OnMouseDown()
@@ -26,19 +20,30 @@ public class DragObject : MonoBehaviour
 
     private void OnMouseDrag()
     {
-        mouseRay = Camera.main.ScreenPointToRay(Input.mousePosition);
-
+        Ray mouseRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+        
         if (basePlane.Raycast(mouseRay, out float distance))
         {
-            // get the hit point:
+            Vector3 toGround = this.transform.TransformDirection(Vector3.down);
+            Debug.DrawRay(this.transform.position, toGround * 50, Color.green);
+
             transform.position = mouseRay.GetPoint(distance);
         }
+
     }
 
     private void OnMouseUp()
     {
         
 
+    }
+    
+    private Plane GetBasePlane()
+    {
+        rb = GetComponent<Rigidbody>();
+        var filter = GameObject.Find("Board").GetComponentInChildren<MeshFilter>();
+        Vector3 normal = filter.transform.TransformDirection(filter.mesh.normals[0]);
+        return new Plane(normal, transform.position);
     }
 }
 
